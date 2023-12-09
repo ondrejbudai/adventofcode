561ffb523214cbeda3f3fdc540fe9cf9f7752db9
Ondřej Budai <ondrej@budai.cz>
2023-12-09 10:59:28 +0100

    day9: extrapolating with elves. Part2 is funny

---
+++ day9p2/src/main.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+fn main() {
+    let f = File::open("input").unwrap();
+    let f = BufReader::new(f);
+    let mut lines = f.lines();
+
+    let sum: i32 = lines.map(|line| {
+        let line = line.unwrap();
+
+        let mut nums: Vec<i32> = line.split(" ").map(|num| num.parse().unwrap()).collect();
+        nums.reverse();
+        let mut lasts = vec![nums.last().unwrap().clone()];
+
+
+        loop {
+            let mut new_nums: Vec<i32> = nums.windows(2).map(|x| x[1] - x[0]).collect();
+
+            nums = new_nums;
+
+            lasts.push(nums.last().unwrap().clone());
+
+
+            if nums.iter().all(|x| *x == 0) {
+                break
+            }
+        }
+
+        lasts.iter().fold(0, |acc, c| acc+ c)
+    }).sum();
+
+    println!("{sum}");
+}
